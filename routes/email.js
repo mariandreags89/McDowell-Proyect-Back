c561e263fa7017e6001c170fd093ce3899cf3568
PauloBazan <paulobazan155@gmail.com>
2023-02-08 21:53:34 +0100

    cambio rutas pdf y email

--- routes/email.js
+++ routes/email.js
@@ -1,6 +1,6 @@
 var router = require('express').Router();
 const { response } = require('express');
-const { postEmailOrder } = require('../controllers/emailController')
+const { postEmailOrder } = require('../controllers/mailControllers/emailController')
 
 router.post('/:id/:email', postEmailOrder);
 module.exports = router;
