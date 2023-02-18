b5dbf2c09e7c11225244871a07719ecef69ad6c4
gema <gjcuenca@hotmail.com>
2023-02-19 00:22:17 +0100

    consulta estados

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 var router = require('express').Router();
+
+
 router.use('/products', require('./products'));
 router.use('/users', require('./users'));
 router.use('/orders', require('./orders'));
+router.use('/status', require('./status'));
 
 
 module.exports = router;
---
+++ routes/status.js
@@ -0,0 +1,5 @@
+var router = require('express').Router();
+
+router.get('/:in_status', require('../controllers/statusControllers/getStatusIn'));
+
+module.exports = router;
