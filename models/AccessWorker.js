d32e17dc2a95407d40fd81f68e4f77ec48cae91e
gema <gjcuenca@hotmail.com>
2023-02-20 20:28:57 +0100

    AccessWorker

--- models/AccessWorker.js
+++ models/AccessWorker.js
@@ -18,19 +18,14 @@ pgClient = startConnection()
 
 class AccessManager {
   
-  static async getworkers() {
-      const queryResponse = await pgClient.query("select users.*, '3,4,5' as access from users right join waiter  on users.id_user= waiter.id_user union select users.*, '1,2,3' as access from users right join chef  on users.id_user= chef.id_user union select users.*, '1,2,3,4,5' as access from users right join admin  on users.id_user= admin.id_user) as worker");
-      const access = convertStatusDataToObjects(queryResponse.rows);
-      return access;
-    }
 
-  static async getAccess(id_users) {
-      const myquery=`"select * from (select users.*, '3,4,5' as state from users right join waiter  on users.id_user= waiter.id_user 
-      union select users.*, '1,2,3' as state from users right join chef  on users.id_user= chef.id_user
-      union select users.*, '1,2,3,4,5' as state from users right join admin  on users.id_user= admin.id_user
-      ) as worker where worker.id_user=$1",[id_user]`;
-       console.log(myquery);
-      const queryResponse = await pgClient.query(myquery);
+  static async getAccess(user) {
+      // const myquery=`"select * from (select users.*, '3,4,5' as state from users right join waiter  on users.id_user= waiter.id_user 
+      // union select users.*, '1,2,3' as state from users right join chef on users.id_user= chef.id_user
+      // union select users.*, '1,2,3,4,5' as state from users right join admin  on users.id_user= admin.id_user
+      // ) as worker where worker.id_user=$1",[id_user]`;
+      //  console.log(myquery);
+      const queryResponse = await pgClient.query("select * from (select users.*, '3,4,5' as state from users right join waiter  on users.id_user= waiter.id_user union select users.*, '1,2,3' as state from users right join chef on users.id_user= chef.id_user union select users.*, '1,2,3,4,5' as state from users right join admin on users.id_user= admin.id_user ) as worker where worker.id_user=$1",[user]);
       const access = convertAccessToObjects(queryResponse.rows);
       return access;
   }
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ router.use('/products', require('./products'));
 router.use('/users', require('./users'));
 router.use('/orders', require('./orders'));
 router.use('/status', require('./status'));
+router.use('/worker', require('./worker'));
 
 
 module.exports = router;
