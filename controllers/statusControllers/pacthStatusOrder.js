b54cdfbf13586418f5753135c2472c3f148840e4
gema <gjcuenca@hotmail.com>
2023-02-27 17:57:21 +0100

    correccion mensaje

--- controllers/statusControllers/pacthStatusOrder.js
+++ controllers/statusControllers/pacthStatusOrder.js
@@ -4,7 +4,7 @@ const pacthStatusOrder = async (req, res) => {
 
     const id = req.params.id;
     await orderStatusManager.pacthStatus(id);
-    res.status(201).json(`Se ha cambiado el estado al pedido: ${id}}`);
+    res.status(201).json(`Se ha cambiado el estado al pedido: ${id}`);
   };
 
 
